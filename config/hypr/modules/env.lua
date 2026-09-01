ed475609ff8eca0a53098352333867f902927967
aprianwdh <aprianwidhiwibowo@gmail.com>
2026-09-01 09:40:59 +0700

    feat: refactor hyprland config

---
+++ config/hypr/modules/env.lua
@@ -0,0 +1,9 @@
+-------------------------------
+---- ENVIRONMENT VARIABLES ----
+-------------------------------
+
+hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("XCURSOR_THEME", "catppuccin-mocha-mauve-cursors")
+hl.env("XCURSOR_SIZE", "24")
+hl.env("GTK_THEME", "catppuccin-mocha-mauve-standard")
---
+++ config/hypr/modules/look.lua
@@ -0,0 +1,51 @@
+-----------------------
+--------- LOOK --------
+-----------------------
+
+-- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
+hl.config({
+    general = {
+        gaps_in = 5,
+        gaps_out = 20,
+
+        border_size = 2,
+
+        col = {
+            active_border = "rgb(136, 57, 239)",
+            inactive_border = "rgba(595959aa)",
+        },
+
+        -- Set to true to enable resizing windows by clicking and dragging on borders and gaps
+        resize_on_border = false,
+
+        -- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
+        allow_tearing = false,
+    },
+
+    decoration = {
+        rounding = 0,
+        rounding_power = 2,
+
+        -- Change transparency of focused and unfocused windows
+        active_opacity = 1.0,
+        inactive_opacity = 0.8,
+
+        shadow = {
+            enabled = false,
+            range = 4,
+            render_power = 3,
+            color = "0xee1a1a1a",
+        },
+
+        blur = {
+            enabled = true,
+            size = 5,
+            passes = 1,
+            vibrancy = 0.1696,
+        },
+    },
+
+    animations = {
+        enabled = true,
+    },
+})
