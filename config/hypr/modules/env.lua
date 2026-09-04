e81ad38a4cc6b8cfdcde9ca76afb31c3c9edcb19
aprianwdh <aprianwidhiwibowo@gmail.com>
2026-09-04 20:42:00 +0700

    feat: change color schame

--- config/hypr/modules/env.lua
+++ config/hypr/modules/env.lua
@@ -4,6 +4,3 @@
 
 hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
-hl.env("XCURSOR_THEME", "catppuccin-mocha-mauve-cursors")
-hl.env("XCURSOR_SIZE", "24")
-hl.env("GTK_THEME", "catppuccin-mocha-mauve-standard")
